8f21c1c329da43a0e0e84aaecfc482b2a1ee76ca
wangrl <wangrl@ckt.cn>
2020-10-10 15:52:06 +0800

    定义完常见的变量

--- runner.py
+++ runner.py
@@ -1,12 +1,60 @@
 import argparse
+import signal
+import sys
+import threading
+
+from src import phone
+from src.info import packages, apps, high_serials
+
+
+def run(pid):
+    print(pid)
 
 
 def main(args):
-    print(args.serial)
+    # 获取设备号
+    devices = []
+    if args.serial:
+        for s in args.serial.split(' '):
+            devices.append(s)
+    else:
+        devices = phone.get_devices()
+    print(devices)
+
+    # 为每部设备创建单独的线程运行
+    threads = []
+    # 设备号对应的线程号
+    pts = {}
+    for pid in devices:
+        if high_serials.__contains__(pid):
+            t = threading.Thread(target=run, args=(pid,), daemon=True)
+            threads.append(t)
+            pts[pid] = t.ident
+            t.start()
+        else:
+            # 跑低配置的手机
+            return None
+
+    # noinspection PyUnusedLocal
+    def signal_handler(sig, frame):
+        # 结束前关闭所有程序
+        for d in devices:
+            activities = phone.get_top_activities(d)
+            for a in apps:
+                if activities.__contains__(packages[a]):
+                    phone.stop_app(d, packages[a], 0.2)
+        sys.exit(0)
+
+    # 等待所有线程退出
+    for t in threads:
+        t.join()
+
+    # 处理中断情况
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.pause()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='PROG', conflict_handler='resolve')
     parser.add_argument('-s', '--serial', help='phone serial number')
-    parser.add_argument('-h', '--high', default='high', help='high/low performance phone')
     main(parser.parse_args())
---
+++ src/info.py
@@ -0,0 +1,59 @@
+# 屏幕的实际尺寸
+# 通过实际测量手机获得
+WIDTH = 0.0
+HEIGHT = 0.0
+
+phone = {
+    'vivo-u3x': ['720x1544']
+}
+
+# 程序名
+apps = ['toutiao', 'kuaishou', 'douyin', 'huoshan',
+        'jingdong', 'fanqie', 'fanchang', 'weishi',
+        'shuqi', 'yingke', 'kugou', 'huitoutiao',
+        'zhongqing', 'pinduoduo', 'taobao', 'shuabao']
+
+# 程序对应的包名
+packages = {
+    apps[0]: 'com.ss.android.article.lite',
+    apps[1]: 'com.kuaishou.nebula',
+    apps[2]: 'com.ss.android.ugc.aweme.lite',
+    apps[3]: 'com.ss.android.ugc.livelite',
+    apps[4]: 'com.jd.jdlite',
+    apps[5]: 'com.dragon.read',
+    apps[6]: 'com.xs.fm',
+    apps[7]: 'com.tencent.weishi',
+    apps[8]: 'com.shuqi.controller',
+    apps[9]: 'com.ingkee.lite',
+    apps[10]: 'com.kugou.android.elder',
+    apps[11]: 'com.cashtoutiao',
+    apps[12]: 'cn.youth.news',
+    apps[13]: 'com.xunmeng.pinduoduo',
+    apps[14]: 'com.taobao.litetao',
+    apps[15]: 'com.jm.video'
+}
+
+activities = {
+    apps[0]: 'com.ss.android.article.lite/.activity.SplashActivity',
+    apps[1]: 'com.kuaishou.nebula/com.yxcorp.gifshow.HomeActivity',
+    apps[2]: 'com.ss.android.ugc.aweme.lite/com.ss.android.ugc.aweme.splash.SplashActivity',
+    apps[3]: 'com.ss.android.ugc.livelite/com.ss.android.ugc.live.main.MainActivity',
+    apps[4]: '#com.jd.jdlite/.MainFrameActivity',
+    apps[5]: '#com.dragon.read/.pages.main.MainFragmentActivity',
+    apps[6]: 'com.xs.fm/com.dragon.read.pages.main.MainFragmentActivity',
+    apps[7]: 'com.tencent.weishi/com.tencent.oscar.module.splash.SplashActivity',
+    apps[8]: '#com.shuqi.controller/com.shuqi.activity.MainActivity',
+    apps[9]: '#com.ingkee.lite/com.meelive.ingkee.business.main.entry.legacy.MainActivity',
+    apps[10]: '#com.kugou.android.elder/com.kugou.android.app.MediaActivity',
+    apps[11]: 'com.cashtoutiao/.account.ui.main.MainTabActivity',
+    apps[12]: '#cn.youth.news/.ui.splash.SplashAdActivity',
+    apps[13]: '#com.xunmeng.pinduoduo/.ui.activity.HomeActivity',
+    apps[14]: 'com.taobao.litetao/com.taobao.ltao.maintab.MainFrameActivity',
+    apps[15]: 'com.jm.video/.ui.main.MainActivity'
+}
+
+numbers = ['15391503757', '17343162426', '15334195745']
+
+# 高配置和低配置手机
+high_serials = ['1fc4da61']
+low_serials = []
