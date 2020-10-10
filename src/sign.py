138e34a5193f42d98e97cb174551697b1783a8cc
wangrl2016 <wangrl2016@gmail.com>
2020-10-11 01:22:23 +0800

    完成番茄畅听签到

--- src/sign.py
+++ src/sign.py
@@ -65,9 +65,23 @@ def fanqie(pid, w, h):
     # 签到成功
     input.tap(pid, w / 2, (HEIGHT - 0.5) * h / HEIGHT)
     # 2. 点击看视频再领金币
-    input.tap(pid, w / 2, 10.1 * h / HEIGHT)
+    input.tap(pid, w / 2, 10.1 * h / HEIGHT)  # <= modify
     # 3. 播放视频30s
     time.sleep(30)
     # 4. 点击关闭
     # 无法回退关闭
-    input.tap(pid, (WIDTH - 0.7) * w / WIDTH, 1.2 * h / HEIGHT)
+    input.tap(pid, (WIDTH - 0.7) * w / WIDTH, 1.2 * h / HEIGHT)  # <= modify
+
+
+def fanchang(pid, w, h):
+    # 1. 点击福利签到
+    # 签到成功
+    input.tap(pid, 4.8 * w / WIDTH, (HEIGHT - 0.5) * h / HEIGHT)  # <= modify
+    # 2. 点击看视频再领金币
+    input.tap(pid, w / 2, 10.1 * h / HEIGHT)  # <= modify
+    # 3. 播放视频30s
+    time.sleep(30)
+    # 4. 点击关闭
+    # 无法回退关闭
+    input.tap(pid, (WIDTH - 0.7) * w / WIDTH, 1.2 * h / HEIGHT)  # <= modify
+
