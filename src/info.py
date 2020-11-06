b9db10732f324340a8131cb8774d0be4ccc21c76
wangrl2016 <wangrl2016@gmail.com>
2020-11-06 14:01:56 +0800

    定位24个程序

--- src/info.py
+++ src/info.py
@@ -6,24 +6,24 @@ HEIGHT = 14.6
 
 phones = {
     'vivo-u3x': ['720x1544'],
-    'Redmi8A': ['720x1544'],
+    'Redmi8A': ['720x1520'],
 }
 
 numbers = ['15391503757', '15334195745', '17343162426', ]
 
 # 低配置手机前面用##表示
-high_serials = ['1fc4da61', 'ce7f96a00307', 'KVLZB65T95VKQOS8']
+high_serials = ['1fc4da61', 'ce7f96a00307']
 
 # 保留运行信息
 contexts = {}
 
 # 程序名
 apps = ['toutiao', 'kuaishou', 'douyin', 'huoshan',
-        'jingdong', 'fanqie', 'fanchang', 'weishi',
+        'jingdong', 'fanqie', 'fanchang', 'kuchang',
         'shuqi', 'yingke', 'kugou', 'huitoutiao',
-        'zhongqing', 'pinduoduo', 'taobao', 'shuabao',
-        'qutoutiao', 'baidu', 'ximalaya', 'douhuo',
-        'kuge', 'makan', 'diandian',
+        'zhongqing', 'pinduoduo', 'kuaiyin', 'tangdou',
+        'dongfang', 'jukandian', 'kankuai', 'douhuo',
+        'kuge', 'makan', 'diandian', 'moji',
         ]
 
 # 程序对应的包名
@@ -35,22 +35,29 @@ packages = {
     apps[4]: 'com.jd.jdlite',
     apps[5]: 'com.dragon.read',
     apps[6]: 'com.xs.fm',
-    apps[7]: 'com.tencent.weishi',
+    apps[7]: 'com.kugou.android.douge',
+    # apps[7]: 'com.tencent.weishi',
     apps[8]: 'com.shuqi.controller',
     apps[9]: 'com.ingkee.lite',
     apps[10]: 'com.kugou.android.elder',
     apps[11]: 'com.cashtoutiao',
     apps[12]: 'cn.youth.news',
     apps[13]: 'com.xunmeng.pinduoduo',
-    apps[14]: 'com.taobao.litetao',
-    apps[15]: 'com.jm.video',
-    apps[16]: 'com.jifen.qukan',
-    apps[17]: 'com.baidu.searchbox.lite',
-    apps[18]: 'com.ximalaya.ting.lite',
+    apps[14]: 'com.kuaiyin.player',
+    # apps[14]: 'com.taobao.litetao',
+    apps[15]: 'com.bokecc.dance',
+    # apps[15]: 'com.jm.video',
+    apps[16]: 'com.songheng.eastnews',
+    # apps[16]: 'com.jifen.qukan',
+    apps[17]: 'com.xiangzi.jukandian',
+    # apps[17]: 'com.baidu.searchbox.lite',
+    apps[18]: 'com.tencent.reading',
+    # apps[18]: 'com.ximalaya.ting.lite',
     apps[19]: 'com.ss.android.ugc.live',
     apps[20]: 'com.kugou.android.child',
     apps[21]: 'com.ldzs.zhangxin',
     apps[22]: 'com.yingliang.clicknews',
+    apps[23]: 'com.moji.mjweather.light',
 }
 
 activities = {
@@ -61,24 +68,30 @@ activities = {
     apps[4]: '#com.jd.jdlite/.MainFrameActivity',
     apps[5]: '#com.dragon.read/.pages.main.MainFragmentActivity',
     apps[6]: '#com.xs.fm/com.dragon.read.pages.main.MainFragmentActivity',
-    apps[7]: 'com.tencent.weishi/com.tencent.oscar.module.splash.SplashActivity',
+    apps[7]: '#com.kugou.android.douge/com.kugou.android.app.MediaActivity',
+    # apps[7]: 'com.tencent.weishi/com.tencent.oscar.module.splash.SplashActivity',
     apps[8]: '#com.shuqi.controller/com.shuqi.activity.MainActivity',
     apps[9]: '#com.ingkee.lite/com.meelive.ingkee.business.main.entry.legacy.MainActivity',
     apps[10]: '#com.kugou.android.elder/com.kugou.android.app.MediaActivity',
     apps[11]: 'com.cashtoutiao/.account.ui.main.MainTabActivity',
     apps[12]: '#cn.youth.news/.ui.splash.SplashAdActivity',
     apps[13]: '#com.xunmeng.pinduoduo/.ui.activity.HomeActivity',
-    apps[14]: 'com.taobao.litetao/com.taobao.ltao.maintab.MainFrameActivity',
-    apps[15]: 'com.jm.video/.ui.main.MainActivity',
-    apps[16]: 'com.jifen.qukan/com.jifen.qkbase.main.MainActivity',
-    apps[17]: 'com.baidu.searchbox.lite/com.baidu.searchbox.MainActivity',
-    apps[18]: 'com.ximalaya.ting.lite/com.ximalaya.ting.android.host.activity.MainActivity',
+    apps[14]: '#com.kuaiyin.player/.v2.ui.main.MainActivity',
+    # apps[14]: 'com.taobao.litetao/com.taobao.ltao.maintab.MainFrameActivity',
+    apps[15]: 'com.bokecc.dance/.activity.MainActivity',
+    # apps[15]: 'com.jm.video/.ui.main.MainActivity',
+    apps[16]: '#com.songheng.eastnews/com.songheng.eastfirst.common.view.activity.MainActivity',
+    # apps[16]: 'com.jifen.qukan/com.jifen.qkbase.main.MainActivity',
+    apps[17]: '#com.xiangzi.jukandian/.activity.MainActivity',
+    # apps[17]: 'com.baidu.searchbox.lite/com.baidu.searchbox.MainActivity',
+    apps[18]: 'com.tencent.reading/.activity.SplashActivity',
+    # apps[18]: 'com.ximalaya.ting.lite/com.ximalaya.ting.android.host.activity.MainActivity',
     apps[19]: 'com.ss.android.ugc.live/.main.MainActivity',
     apps[20]: '#com.kugou.android.child/com.kugou.android.app.MediaActivity',
     apps[21]: '#com.ldzs.zhangxin/com.weishang.wxrd.activity.MainActivity',
     apps[22]: '#com.yingliang.clicknews/.MainActivity',
+    apps[23]: 'com.moji.mjweather.light/com.moji.mjweather.MainActivity',
 }
 
 # 程序的定时任务为25分钟
-# 主要因为有些任务20分钟一次
 SCHEDULE_TIME = 25
