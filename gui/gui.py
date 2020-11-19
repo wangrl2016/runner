693f2b146c062728ee19fc75ac8648af8baa54dc
wangrl2016 <wangrl2016@gmail.com>
2020-11-19 11:40:12 +0800

    添加获取设备

--- gui/gui.py
+++ gui/gui.py
@@ -1,29 +1,44 @@
+import threading
 import tkinter as tk
 
+import src.phone
+from src import phone
 
-def say_hi():
-    print("hi there, everyone!")
+
+def go_home():
+    print("回到主页")
 
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.hi_there = tk.Button(self)
+        self.back = tk.Button(self, text="返回", command=self.master.destroy)
+        self.home = tk.Button(self)
         self.master = master
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = say_hi
-        self.hi_there.pack(side="top")
+        self.home["text"] = "主页"
+        self.home["command"] = go_home
+        self.home.pack(side="left")
 
-        self.quit.pack(side="bottom")
+        self.back.pack(side="right")
 
 
 if __name__ == '__main__':
     root = tk.Tk()
+
+    devices = phone.get_devices()
+    if not devices:
+        exit(0)
+
+    (w, h) = phone.get_size(devices[0])
+
+    scale = 0.5
+
+    frame = tk.Frame(root, width=w * scale, height=h * scale, bg='white')
+    frame.pack()
+
     app = Application(master=root)
     app.mainloop()
