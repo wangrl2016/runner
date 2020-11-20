e79e3eea97a0cc3e5542e23a2ec51b27a8484286
wangrl2016 <wangrl2016@gmail.com>
2020-11-20 14:25:32 +0800

    完成input事件

--- src/input.py
+++ src/input.py
@@ -2,6 +2,9 @@ import subprocess
 import time
 from random import randrange
 
+LANDSCAPE_OFFSET = 15
+VERTICAL_OFFSET = 5
+
 """
 The commands and default sources are:
     text <string> (Default: touchscreen)
@@ -12,37 +15,57 @@ The commands and default sources are:
 """
 
 
-def text(pid, txt):
+def text(pid, txt, gap=3):
     """
     输入文字
+
     :param pid: 手机标识符
     :param txt: 想要输入的文本
+    :param gap: 时间间隔
     """
     subprocess.run(['adb', '-s', pid, 'shell', 'input', 'text', txt])
-    time.sleep(3)
+    time.sleep(gap)
 
 
-def keyevent(pid, code, gap=3):
+def keyevent(pid, event, gap=3):
     """
-    按键事件
+    输入按键事件
+    https://developer.android.com/reference/android/view/KeyEvent
+
+    :param pid: 手机标识符
+    :param event: 事件
+    :param gap: 时间间隔
     """
-    subprocess.run(['adb', '-s', pid, 'shell', 'input', 'keyevent', code])
+    subprocess.run(['adb', '-s', pid, 'shell', 'input', 'keyevent', event])
     time.sleep(gap)
 
 
 def tap(pid, w, h, gap=5):
     """
-    以像素点点击屏幕坐标为(px, py)的位置
+    点击手机像素坐标点
+
+    :param pid: 手机标识符
+    :param w: 水平方向像素坐标
+    :param h: 竖直方向像素坐标
+    :param gap: 时间间隔
     """
-    w = w + randrange(-10, 10)
-    h = h + randrange(-5, 5)
+    w = w + randrange(-LANDSCAPE_OFFSET, VERTICAL_OFFSET)
+    h = h + randrange(-VERTICAL_OFFSET, VERTICAL_OFFSET)
     subprocess.run(['adb', '-s', pid, 'shell', 'input', 'tap', str(int(w)), str(int(h))])
     time.sleep(gap)
 
 
 def swipe(pid, w1, h1, w2, h2, internal=200, gap=3):
     """
-    滑动事件
+    滑动屏幕
+
+    :param pid: 手机标识符
+    :param w1: 第一个点水平方向像素坐标
+    :param h1: 第一个点竖直防线像素坐标
+    :param w2: 第二个点水平方向像素坐标
+    :param h2: 第二个点竖直方向像素坐标
+    :param internal: 两个点之间的滑动间隔时间
+    :param gap: 时间间隔
     """
     subprocess.run(['adb', '-s', pid, 'shell', 'input', 'swipe',
                     str(int(w1)), str(int(h1)),
