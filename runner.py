39a2e120f77a4d6663cf21675434d0fa867e066a
wangrl2016 <wangrl2016@gmail.com>
2020-11-22 20:31:53 +0800

    修正运行问题

--- runner.py
+++ runner.py
@@ -36,7 +36,9 @@ def cycle(pid):
     while True:
         hour = datetime.now().hour
         if run_apps.__contains__('kuaishou') and (hour == 1 or hour == 2 or hour == 13 or hour == 14 or hour == 17):
-            checkin.kuaishou(pid)
+            checkin.kuaishou(pid, gap=20)
+            # 回退可能出现的悬浮窗
+            phone.go_back(pid)
             app.watch_kuaishou_video(pid, w, h, hour)
             phone.stop_app(pid, info.packages['kuaishou'])
         else:
