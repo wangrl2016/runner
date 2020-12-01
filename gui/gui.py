c3863d5db6d60bd7e68d7fc4e114668f377edd14
wangrl2016 <wangrl2016@gmail.com>
2020-12-01 10:41:11 +0800

    正确处理输出

--- gui/gui.py
+++ gui/gui.py
@@ -7,6 +7,7 @@ from PIL import Image, ImageTk
 from src import phone
 import cairosvg
 from io import BytesIO
+import shutil
 
 
 class Application(tk.Frame):
@@ -64,11 +65,6 @@ class Application(tk.Frame):
         self.create_widgets()
 
         self.prev_img = None
-        self.curr_img = None
-
-    # def destroy(self):
-    #     self.master.destroy()
-    #     os.remove('../out/' + self.curr_img)
 
     def create_widgets(self):
         self.image_frame.pack_propagate(0)  # 固定frame的大小
@@ -131,16 +127,14 @@ class Application(tk.Frame):
             phone.go_back(pid)
 
     def update_page(self):
-        print('更新页面 ' + datetime.now().time().__str__())
-        file_path = phone.get_page_photo(devices[0], '../out/')
-        self.curr_img = file_path
-        img = Image.open('../out/' + file_path).resize((int(w * scale), int(h * scale)))
+        file_path = phone.get_page_photo(devices[0], out_dir)
+        img = Image.open(out_dir + file_path).resize((int(w * scale), int(h * scale)))
         img = ImageTk.PhotoImage(image=img)
         self.image_label.config(image=img)
         self.image_label.image = img
 
         if self.prev_img:
-            os.remove('../out/' + file_path)
+            os.remove(out_dir + file_path)
 
         self.prev_img = file_path
 
@@ -161,6 +155,8 @@ if __name__ == '__main__':
         print('没有发现手机设备')
         exit(0)
 
+    out_dir = '../out/'
+
     (w, h) = phone.get_size(devices[0])
 
     scale = 0.5
@@ -170,3 +166,6 @@ if __name__ == '__main__':
     root.after(1000, app.update_page)
 
     app.mainloop()
+
+    shutil.rmtree(out_dir)
+    os.mkdir(out_dir)
